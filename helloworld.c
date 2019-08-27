1c206ca8d882d1166b88efe784a31880575f34d4
Prabhakar Sarma Neog <psneog@gmail.com>
2019-08-28 00:33:32 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,25 @@
+/**************************************
+Preprocessor directives
+*****************************************/
+
+# include <stdio.h>  // this header file contains prototypes and other info about 
+                      // basic i/o functions like printf/scanf etc.
+                      
+/****************************************************
+Global variables and constants 
+these can be used in the rest part of code
+******************************************************
+
+/*********************************************
+The main() is the function that is to be run first.
+
+*********************************************/
+
+void main()  {
+       // the customary
+       printf("Hello world!\n")
+}
+
+/***********************************************
+....and  thats it!
+****************************/
