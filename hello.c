b10a58d29c00ebb8718459d99d0abae1c40f9582
Prabhaka Sarma Neog <psneog@gmail.com>
2022-01-03 10:56:32 +0530

    Commit by psneog

---
+++ hello.c
@@ -0,0 +1,23 @@
+// Program to display Hello World
+// Single line comment
+
+
+/*  
+      this is a multiline/block comment
+      another line
+*/
+
+
+// precompiler directives starts with a # at column 1
+# include <stdio.h>
+
+// the main() function
+// the main() function is the 
+// entry point to any c program
+
+int main() 
+{
+	printf("Hello world\n");   // displays Hello world
+	return(0);                 // exit from main()
+}	// end of main
+
