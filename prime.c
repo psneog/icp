b3bcffeaf935040e56c825362eafaca4a000b718
Prabhaka Sarma Neog <psneog@gmail.com>
2022-01-06 15:22:38 +0530

    Primes!

---
+++ prime.c
@@ -0,0 +1,40 @@
+/********************************************
+Program to determine whether a number n is
+prime or not
+*********************************************/
+
+# include <stdio.h>
+
+void main() {
+	
+	int n = 1;	// number to be tested
+	
+	int i = 2;	// will test n by dividing
+			// by i
+			
+	int flag = 1; 	// boolean flag
+			// will be set to logical 
+			// false when n is found
+			// to be not prime
+		
+	printf("Please enter n: ");
+	scanf("%d", &n);
+	
+	// test for divisibility of n by i
+	// by verying i from 2 to n-1
+	while( (i < n) && flag) {
+	    
+	    // divisibility test 	   
+	    if (n % i == 0) {
+	        flag = 0;
+	    } // if
+	    i++;
+	} // while
+	
+	if(flag)
+	    printf("%d is prime\n", n);
+	else
+	    printf("%d is not prime\n", n);
+	    
+	return;
+}
